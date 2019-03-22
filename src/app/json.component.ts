d84212977e846c3a79b9b7759e7ff37dc10d3c43
yentrapragada1991 <praveen28.tableau@gmail.com>
2019-03-22 13:00:34 +0530

    second commit

--- src/app/json.component.ts
+++ src/app/json.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./json.component.css']
 })
 export class JsonComponent implements OnInit {
-  facultyinfo={"name":"rlp","course":["mvc","anjular"]};
+  facultyinfo={"name":"praveen","course":["mvc","anjular"]};
   prods=[{"pid":"p001","name":"infosys","price":4000,"pic":"infosys-logo-JPEG.jpg"},
 {"pid":"p002","name":"sample","price":3000,"pic":"sample.jpg"},
 {"pid":"p003","name":"sample1","price":2000,"pic":"sample1.jpg"}]
